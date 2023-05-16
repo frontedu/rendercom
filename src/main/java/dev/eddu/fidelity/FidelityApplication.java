8621aaa07bcd006b4d5466867225123be6d4ee2f
Eddu <frontedu@icloud.com>
2023-05-16 03:17:59 +0000

    test

--- src/main/java/dev/eddu/fidelity/FidelityApplication.java
+++ src/main/java/dev/eddu/fidelity/FidelityApplication.java
@@ -2,6 +2,8 @@ package dev.eddu.fidelity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class FidelityApplication {
@@ -9,5 +11,13 @@ public class FidelityApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FidelityApplication.class, args);
 	}
+
+@RestController
+class Controller{
+	@GetMapping("/")
+	private String index(){
+		return "Hello World";
+	}
+}
 
 }
